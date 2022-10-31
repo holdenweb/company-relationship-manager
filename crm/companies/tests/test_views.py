623de6a14eb4015b175564d81f9900d055519eaa
Henry Cooksley <hccooksley@deloitte.co.uk>
2022-10-31 15:21:36 +0000

    Update tests for views

--- crm/companies/tests/test_views.py
+++ crm/companies/tests/test_views.py
@@ -1,34 +1,6 @@
+from os import stat
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
-
-
-# class CompaniesViewsTest(APITestCase):
-#     fixtures = ["books.json", "users.json"]
-
-#     def setUp(self):
-#         self.client = APIClient()
-
-#     def test_get_all_books(self):
-#         response = self.client.get("/books/")
-
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertContains(response, "Moby-Dick")
-#         self.assertContains(response, "Architecture Patterns with Python")
-#         self.assertContains(response, "The Oxford Handbook of Happiness")
-
-#     def test_get_a_book(self):
-#         response = self.client.get(f"/books/1/")
-
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertContains(response, "Moby-Dick")
-#         self.assertNotContains(response, "Architecture Patterns with Python")
-#         self.assertNotContains(response, "The Oxford Handbook of Happiness")
-
-#     def test_get_all_books_json(self):
-#         response = self.client.get("/books.json")
-
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(response["content-type"], "application/json")
 
 
 class CompaniesViewsTest(APITestCase):
@@ -41,3 +13,20 @@ class CompaniesViewsTest(APITestCase):
         response = self.client.get("/companies/")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, "ABC Computers")
+        self.assertContains(response, "Capital Partners")
+        self.assertContains(response, "Royal Institute of Trade Studies")
+
+    def test_get_a_company(self):
+        response = self.client.get("/companies/1/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, "ABC Computers")
+        self.assertNotContains(response, "Capital Partners")
+        self.assertNotContains(response, "Royal Institute of Trade Studies")
+
+    def test_get_all_companies_json(self):
+        response = self.client.get("/companies.json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response["content-type"], "application/json")
