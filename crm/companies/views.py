a7bbded5ffc849f1031f85e33a6639a57ea93bd9
Henry Cooksley <hccooksley@deloitte.co.uk>
2022-10-31 15:30:01 +0000

    Update views

--- crm/companies/views.py
+++ crm/companies/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from companies.models import Company
 from companies.serializers import CompanySerializer
@@ -14,3 +16,10 @@ class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response(
+        {"companies": reverse("companies-list", request=request, format=format)}
+    )
